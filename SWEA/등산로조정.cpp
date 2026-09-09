9c98ab7a60de9cdc081ca3f5e01b7cbebcab826e
kittty <ccjsgh@naver.com>
2026-09-09 10:11:47 +0900

    hehe

--- SWEA/등산로조정.cpp
+++ SWEA/등산로조정.cpp
@@ -1,88 +1,96 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
-#include<cmath>
 
 using namespace std;
 
+
+
+
 vector<vector<int>> v;
-vector<pair<int,int>> t; 
 vector<vector<bool>> visited;
+vector<pair<int,int>> t;
+
+int N, K;
+
+int total_len =0;
 
 int dr[4] = {-1,1,0,0};
 int dc[4] = {0,0,-1,1};
 
-int N, K;
-int max_len=0;
-void backtrack(int r, int c, int cnt, bool used){
-  max_len = max(max_len, cnt);
+void backtrack(int r, int c, int cnt, bool dig ){
+  total_len = max(total_len, cnt);
+  
+  for (int i =0; i<4; i++){
+    int nr = r + dr[i];
+    int nc = c + dc[i];
 
-  for (int i=0; i<4; i++){
-    int nr = r +dr[i];
-    int nc = c +dc[i];
     if (nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
 
     if (v[nr][nc] < v[r][c] && !visited[nr][nc]){
-      visited[nr][nc]=true;
-      backtrack(nr, nc, cnt+1, used);
-      visited[nr][nc]=false;
+      visited[nr][nc] = true;
+      backtrack(nr,nc,cnt+1, dig);
+      visited[nr][nc] = false;
     }
-    else if (!used && v[nr][nc] - K < v[r][c] && !visited[nr][nc]){
+    else if (!dig && v[nr][nc] - K < v[r][c] && !visited[nr][nc] && v[nr][nc] >= v[r][c]){
       int temp = v[nr][nc];
-      v[nr][nc] = v[r][c] - 1;
-      visited[nr][nc]=true;
-      backtrack(nr,nc, cnt+1, true);
-      visited[nr][nc]=false;
+      visited[nr][nc] = true;
+
+      v[nr][nc] = v[r][c] -1;
+      backtrack(nr,nc,cnt+1,true);
+
       v[nr][nc] = temp;
+      visited[nr][nc] = false;
     }
   }
 }
-
 
 int main(int argc, char** argv)
 {
 	int test_case;
 	int T;
-	
 	cin>>T;
-  
+	
 	for(test_case = 1; test_case <= T; ++test_case)
 	{
+
     cin >> N >> K;
 
-    v.assign(N, vector<int>(N,0));
-    max_len = 0;
+    total_len = 0;
 
-    for (int i=0; i< N; i++){
-      for(int j=0; j<N; j++){
-        cin >> v[i][j];
+    v.assign(N,vector<int>(N,0));
+
+    for (int i=0; i<N; i++){
+      for (int j =0; j<N; j++){
+        cin >>v[i][j]; 
       }
     }
 
-    int temp=0;
+    t.clear();
+    int temp=v[0][0];
 
-    for (int i=0; i< N; i++){
-      for(int j=0; j<N; j++){
-        if(v[i][j] > temp){
-          temp = v[i][j];
+    for (int i=0; i<N; i++){
+      for (int j =0; j<N; j++){
+        if(temp < v[i][j]){
           t.clear();
+          temp = v[i][j];
           t.push_back({i,j});
         }
-        else if (v[i][j] == temp){
+        else if (temp == v[i][j]){
           t.push_back({i,j});
         }
       }
     }
 
-    for(int i=0; i< t.size(); i++){
-      visited.assign(N, vector<bool>(N,false));
+    for (int i=0; i<t.size(); i++){
+      visited.assign(N,vector<bool>(N,false));
       visited[t[i].first][t[i].second] = true;
       backtrack(t[i].first, t[i].second, 1, false);
     }
 
+    cout << "#" << test_case << " " << total_len << "\n";
 
-    cout << "#" << test_case << " " << max_len << "\n";
 
-  }
-  return 0;//정상종료시 반드시 0을 리턴해야합니다.   
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
 }
